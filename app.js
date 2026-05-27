1b2de0a8a920050b7451f01718ca0a03d51985a1
André Araya Vargas <93400951+AndreAraya@users.noreply.github.com>
2026-05-26 23:32:44 -0600

    Update greeting message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World desde Node + Express!</h1>');
+  res.send('<h1>Hello World actualizado desde Git!</h1>');
 });
 
 app.listen(3000, () => {
